4b653133fea8c1547646b70703ba7a5307cc314b
Antoine Bédard <antoinebedardqc@gmail.com>
2022-12-04 15:27:14 -0500

    Refactoring et ajout validation pour les lieux au back-end

--- trouve_ta_voie_serveur/controllers/lieuController.js
+++ trouve_ta_voie_serveur/controllers/lieuController.js
@@ -6,8 +6,9 @@ const Lieu = db.lieux;
 const Utilisateur = db.utilisateurs;
 const Grimpe = db.grimpes;
 
-// Obtenir les lieux pour être affiché dans les listes déroulantes.
+
 exports.getDropdownData = async (req, res) => {
+    // Obtenir les lieux pour être affiché dans les listes déroulantes.
     try {
         await Lieu.findAll({attributes: ["id", "titre"]})
             .then(lieux => {
@@ -29,26 +30,23 @@ exports.createLieu = async (req, res) => {
         const locationGEOIsValid = validatorFct.locationGEOIsValid(req.body.latitude, req.body.longitude);
 
         if (locationTitleIsValid && locationDescriptionIsValid && locationInstructionIsValid && locationGEOIsValid) {
-            let user = await Utilisateur.findByPk(req.token.userId);
+            const location = await Lieu.findOne({where: {titre: req.body.titre}});
 
-            if (user) {
-                const location = await Lieu.findOne({where: {titre: req.body.titre}});
-                if (location) {
-                    res.status(400).json({err: "Titre déjà utilisé !"});
-                } else {
-                    await Lieu.create({
-                        titre: req.body.titre,
-                        description: req.body.description,
-                        directives: req.body.directives,
-                        latitude: req.body.latitude,
-                        longitude: req.body.longitude,
-                        utilisateurId: req.token.userId,
-                    }).then(location => {
-                        res.status(201).json(location);
-                    });
-                }
+            // Vérification si le titre est disponible.
+            if (location) {
+                res.status(400).json({err: "Titre déjà utilisé !"});
             } else {
-                res.status(401).end();
+                // Création du lieu.
+                await Lieu.create({
+                    titre: req.body.titre,
+                    description: req.body.description,
+                    directives: req.body.directives,
+                    latitude: req.body.latitude,
+                    longitude: req.body.longitude,
+                    utilisateurId: req.token.userId,
+                }).then(location => {
+                    res.status(201).json(location);
+                });
             }
         } else {
             res.status(400).end();
@@ -78,22 +76,29 @@ exports.getLieuById = async (req, res) => {
 
 exports.getLieuDetailsById = async (req, res) => {
     try {
-        let location = await Lieu.findByPk(+req.params.id);
+        const location = await Lieu.findByPk(+req.params.id);
 
         if (location) {
-            let nbGrimpe = await Grimpe.count({where: {lieuxId: req.params.id}});
+            // Nombre de grimpes que le lieu possède.
+            const nbGrimpe = await Grimpe.count({
+                where: {lieuxId: req.params.id},
+            });
 
+            // edit : Faire en sorte de recuperer ceux qui sont a 0??
+            // Les styles de grimpe (qt) que le lieu possède.
             let grimpeStyle = await Grimpe.findAndCountAll({
                 attributes: ["style"],
                 group: "style",
                 where: {lieuxId: req.params.id},
             });
+            // Les difficultés de grimpe (qt) que le lieu possède.
             let grimpeDiff = await Grimpe.findAndCountAll({
                 attributes: ["difficulte"],
                 group: "difficulte",
                 where: {lieuxId: req.params.id},
                 order: [],
             });
+            // les grimpes que le lieu possède.
             let grimpes = await Grimpe.findAll({where: {lieuxId: req.params.id}});
 
             res.status(200).json({
@@ -117,7 +122,7 @@ exports.getLieuByIdToEdit = async (req, res) => {
             .then(location => {
                 if (!location) {
                     res.status(404).end();
-                } else if (+location.utilisateurId === +req.params.userId) {
+                } else if (+location.utilisateurId === +req.params.userId || req.token.isAdmin) {
                     res.status(200).json(location);
                 } else {
                     res.status(403).end();
@@ -139,34 +144,34 @@ exports.editLieu = async (req, res) => {
         const locationGEOIsValid = validatorFct.locationGEOIsValid(req.body.latitude, req.body.longitude);
 
         if (locationTitleIsValid && locationDescriptionIsValid && locationInstructionIsValid && locationGEOIsValid) {
-            let user = await Utilisateur.findByPk(req.token.userId);
+            let location = await Lieu.findByPk(req.body.id);
 
-            if (user) {
-                let location = await Lieu.findByPk(req.body.id);
+            if (location) {
+                // Possède les autorisations.
+                if (+location.utilisateurId === +req.token.userId || req.token.isAdmin) {
+                    const location2 = await Lieu.findOne({
+                        where: {titre: req.body.titre},
+                    });
 
-                if (location) {
-                    if (+location.utilisateurId === +req.token.userId) {
-                        const location2 = await Lieu.findOne({where: {titre: req.body.titre}});
-                        if (location2 && location2.id !== req.body.id) {
-                            res.status(400).json({err: "Titre déjà utilisé !"});
-                        } else {
-                            await Lieu.update({
-                                titre: req.body.titre,
-                                description: req.body.description,
-                                directives: req.body.directives,
-                                longitude: req.body.longitude,
-                                latitude: req.body.latitude,
-                            }, {where: {id: req.body.id}});
-                            res.status(204).end();
-                        }
+                    // Vérification si le titre est disponible.
+                    if (location2 && location2.id !== req.body.id) {
+                        res.status(400).json({err: "Titre déjà utilisé !"});
                     } else {
-                        res.status(403).end();
+                        await Lieu.update({
+                            titre: req.body.titre,
+                            description: req.body.description,
+                            directives: req.body.directives,
+                            longitude: req.body.longitude,
+                            latitude: req.body.latitude,
+                        }, {where: {id: req.body.id}});
+
+                        res.status(204).end();
                     }
                 } else {
-                    res.status(404).end();
+                    res.status(403).end();
                 }
             } else {
-                res.status(401).end();
+                res.status(404).end();
             }
         } else {
             res.status(400).end();
@@ -178,22 +183,18 @@ exports.editLieu = async (req, res) => {
 
 exports.getLieuxForUserId = async (req, res) => {
     try {
-        if (+req.params.userId !== +req.token.userId)
+        if (+req.params.userId !== +req.token.userId) {
             return res.status(403).end();
-
-        let user = await Utilisateur.findByPk(req.token.userId);
-
-        if (user) {
-            await Lieu.findAll({where: {utilisateurId: req.token.userId}})
-                .then(lieux => {
-                    res.status(200).json(lieux);
-                })
-                .catch(() => {
-                    res.status(400).end();
-                });
-        } else {
-            res.status(400).end();
         }
+
+        // Récupère les lieux de l'utilisateur connecté.
+        await Lieu.findAll({where: {utilisateurId: req.token.userId}})
+            .then(lieux => {
+                res.status(200).json(lieux);
+            })
+            .catch(() => {
+                res.status(400).end();
+            });
     } catch (e) {
         res.status(500).end();
     }
@@ -201,12 +202,20 @@ exports.getLieuxForUserId = async (req, res) => {
 
 exports.getLieuByTitle = async (req, res) => {
     try {
-        let lieu = await Lieu.findOne({where: {titre: req.params.titre}});
-
-        if (lieu) {
-            res.status(200).json(lieu);
+        if (validatorFct.locationTitleIsValid(req.params.titre)) {
+            await Lieu.findOne({
+                where: {titre: req.params.titre},
+            }).then(lieu => {
+                if (lieu) {
+                    res.status(200).json(lieu);
+                } else {
+                    res.status(404).end();
+                }
+            }).catch(() => {
+                res.status(404).end();
+            });
         } else {
-            res.status(404).end();
+            res.status(400).end();
         }
     } catch (e) {
         res.status(500).end();
